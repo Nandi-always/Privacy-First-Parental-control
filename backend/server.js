2f8b3e81a013e88269198fd564f58ce02b35c435
monikakn-git <monikakn115@gmail.com>
2026-02-05 23:15:46 +0530

    Fix PORT for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -44,5 +44,5 @@ mongoose
   .catch(err => console.error(err));
 
 // Start server
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
